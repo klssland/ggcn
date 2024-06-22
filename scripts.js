e324691017d8abf18b708f13ecb681404bdffb03
Nick <28671837+klssland@users.noreply.github.com>
2024-06-22 13:34:28 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('expandButton').addEventListener('click', function() {
-    var hiddenContent = document.getElementById('hiddenContent');
-    if (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') {
-        hiddenContent.style.display = 'block';
-    } else {
-        hiddenContent.style.display = 'none';
-    }
-});
